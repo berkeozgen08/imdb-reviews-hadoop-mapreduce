640b39bd1c705ea877aa56682a475032199b6502
Berke Özgen <berkeozgen08@hotmail.com>
2022-05-09 18:48:39 +0300

    int parse

--- src/main/java/com/mapreduce/movieaverage/MovieAverageMapper.java
+++ src/main/java/com/mapreduce/movieaverage/MovieAverageMapper.java
@@ -14,6 +14,9 @@ public class MovieAverageMapper extends MapReduceBase implements Mapper<LongWrit
 	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
 		String valueString = value.toString();
 		JSONObject obj = JSONParser.parse(valueString);
-		output.collect(new Text(obj.getString("movie")), new IntWritable(obj.getInt("rating")));
+		output.collect(
+			new Text(obj.getString("movie")),
+			new IntWritable(Integer.parseInt(obj.getString("rating")))
+		);
 	}
 }
