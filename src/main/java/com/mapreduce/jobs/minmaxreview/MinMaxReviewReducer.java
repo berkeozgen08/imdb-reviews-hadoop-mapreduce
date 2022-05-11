23e0bb578e046bb6612787a6ff5b42392bc06c17
Berke Özgen <berkeozgen08@hotmail.com>
2022-05-11 16:15:58 +0300

    min max

--- src/main/java/com/mapreduce/jobs/minmaxreview/MinMaxReviewReducer.java
+++ src/main/java/com/mapreduce/jobs/minmaxreview/MinMaxReviewReducer.java
@@ -1,22 +1,29 @@
 package com.mapreduce.jobs.minmaxreview;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Iterator;
 
-import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
 
-public class MinMaxReviewReducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, DoubleWritable> {
-	public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, DoubleWritable> output, Reporter reporter) throws IOException {
-		int total = 0;
-		int count = 0;
+public class MinMaxReviewReducer extends MapReduceBase implements Reducer<Text, MinMaxTuple, Text, MinMaxTuple> {
+	private MinMaxTuple result = new MinMaxTuple();
+
+	public void reduce(Text key, Iterator<MinMaxTuple> values, OutputCollector<Text, MinMaxTuple> output, Reporter reporter) throws IOException {
+		result.setMin(null);
+		result.setMax(null);
 		while (values.hasNext()) {
-			IntWritable value = (IntWritable) values.next();
-			total += value.get();
-			count++;
+			MinMaxTuple minMaxTuple = values.next();
+			if (result.getMax() == null || (minMaxTuple.getMax() > result.getMax())) {
+				result.setMax(minMaxTuple.getMax());
+			}
+			if (result.getMin() == null || (minMaxTuple.getMin() < result.getMin())) {
+				result.setMin(minMaxTuple.getMin());
+			}
 		}
-		output.collect(key, new DoubleWritable(total / (double) count));
+		output.collect(key, result);
 	}
 }
